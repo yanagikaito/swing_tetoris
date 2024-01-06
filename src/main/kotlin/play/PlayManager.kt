a0e228bdcf9c25f3440915a5818f8faf3764bd27
orutanakaito <voltgt777@gmail.com>
2024-01-06 19:04:08 +0900

    playmanagerクラスを作成し,メインプレイエリアをウィンドウの中央に作成したいと思います。

---
+++ src/main/kotlin/play/PlayManager.kt
@@ -0,0 +1,16 @@
+package play
+
+class PlayManager {
+
+    private val width = 360
+
+    private val height = 600
+
+    private val leftX: Int = 0
+
+    private val rightX: Int = 0
+
+    private val topY: Int = 0
+
+    private val bottomY: Int = 0
+}
