d997f73cb82c60e83607495eb7ab66e397952a23
orutanakaito <voltgt777@gmail.com>
2024-01-06 14:51:31 +0900

    ウィンドウを画面に表示するようにしました。

--- src/main/kotlin/gamemain/Main.kt
+++ src/main/kotlin/gamemain/Main.kt
@@ -1,5 +1,19 @@
 package gamemain
 
-class Main {
+import javax.swing.JFrame
 
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val window = JFrame()
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+
+        window.setLocationRelativeTo(null)
+
+        // ウィンドウを画面上に表示します。
+        window.isVisible = true
+    }
 }
