d0cf1b84a1d078721ef233cc31d75be15fe782c5
orutanakaito <voltgt777@gmail.com>
2024-01-06 18:05:26 +0900

    run関数にデルタ式を追加しました。

--- src/main/kotlin/gamemain/GamePanel.kt
+++ src/main/kotlin/gamemain/GamePanel.kt
@@ -32,5 +32,58 @@ class GamePanel : JPanel(), Runnable {
 
     override fun run() {
 
+        /**
+         * デルタまたは累積型方式
+         *
+         * 0.016秒ごとに画面の描画を行います。
+         */
+
+        val drawInterval = (1000000000 / fps).toDouble()
+        var delta = 0.0
+        var lastTime = System.nanoTime()
+        var currentTime: Long
+
+        /**
+         * アップデートと描画が実行されるたびに,このdrawCountに1を追加します。
+         */
+
+        var timer: Long = 0
+        var drawCount = 0
+
+        while (gameThread != null) {
+
+            /**
+             * 1ループごとに
+             *
+             * updateとrepaintを実行し,
+             *
+             * 経過時間をtimerに追加していきます。
+             */
+
+            currentTime = System.nanoTime()
+            delta += (currentTime - lastTime) / drawInterval
+            timer += (currentTime - lastTime)
+            lastTime = currentTime
+            if (delta >= 1) {
+                update()
+                repaint()
+                delta--
+                drawCount++
+            }
+
+            if (timer >= 1000000000) {
+                println("FPS:$drawCount")
+                println("処理に要した時間 = $timer")
+                println(timer / 1000000000)
+                println("秒")
+                // drawCountとtimerをリセットします。
+                drawCount = 0
+                timer = 0
+            }
+        }
+    }
+
+    fun update() {
+
     }
 }
