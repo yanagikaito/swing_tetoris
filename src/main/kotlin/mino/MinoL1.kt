1721663d7bf474b145fdad995e42223cca5484b8
orutanakaito <voltgt777@gmail.com>
2024-01-09 01:31:20 +0900

    minol1のブロックの大きさを設定しました。

--- src/main/kotlin/mino/MinoL1.kt
+++ src/main/kotlin/mino/MinoL1.kt
@@ -1,4 +1,48 @@
 package mino
 
-class MinoL1 {
+import java.awt.Color
+
+private val Block.blockSize: Any
+    get() {
+        TODO("Not yet implemented")
+    }
+
+class MinoL1(private var block: Block) : Mino() {
+
+    init {
+
+        this.block = block
+
+        create(Color.orange)
+    }
+
+    override fun setXY(x: Int, y: Int) {
+
+        getB()[0].x = x
+
+        getB()[1].y = y
+
+        getB()[1].x = getB()[0].x
+
+        getB()[1].y = getB()[0].y - block.blockSize
+
+        getB()[2].x = getB()[0].x
+
+        getB()[2].y = getB()[0].y + block.blockSize
+
+        getB()[3].x = getB()[0].x + block.blockSize
+
+        getB()[3].y = getB()[0].y + block.blockSize
+    }
 }
+
+private operator fun Int.minus(blockSize: Any): Int {
+
+    return blockSize as Int
+}
+
+private operator fun Int.plus(blockSize: Any): Int {
+
+    return blockSize as Int
+}
+
